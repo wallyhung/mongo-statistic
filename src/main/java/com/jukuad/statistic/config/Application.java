a383aa1342162e6273ab0468d67d2fee9573d6c0
hongwei <hongwei200612@gmail.com>
2014-04-22 23:40:08 +0800

    spring自动注入

--- src/main/java/com/jukuad/statistic/config/Application.java
+++ src/main/java/com/jukuad/statistic/config/Application.java
@@ -1,21 +1,21 @@
 package com.jukuad.statistic.config;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@ComponentScan
 public class Application {
 	
-	public static ApplicationContext ctx;
     public static void main(String[] args) 
     {
 //        ctx = SpringApplication.run(Application.class, args);
-    	ApplicationContext  ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-		String[] ss = ctx.getBeanDefinitionNames();
-		for (String s : ss) {
-			System.out.println(s);
-		}
+//    	ApplicationContext  ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+//		String[] ss = ctx.getBeanDefinitionNames();
+//		for (String s : ss) {
+//			System.out.println(s);
+//		}
+    	
+    	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    	context.register(SpringMongoConfig.class);
+    	context.refresh();
     }
 
 }
---
+++ src/main/java/com/jukuad/statistic/config/SpringMongoConfig.java
@@ -0,0 +1,68 @@
+package com.jukuad.statistic.config;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.jukuad.statistic.base.DatastoreFactoryBean;
+import com.jukuad.statistic.base.MongoClientOptionsBean;
+import com.jukuad.statistic.base.MongoFactoryBean;
+import com.jukuad.statistic.base.MorphiaFactoryBean;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClientOptions;
+
+@Configuration
+public class SpringMongoConfig 
+{
+	public @Bean MongoClientOptions mongoClientOptions()
+	{
+		MongoClientOptions options = null;
+		try {
+			options = new MongoClientOptionsBean().getObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return options;
+	}
+	
+	public @Bean Mongo mongo()
+	{
+		Mongo client = null;
+		try {
+			client = new MongoFactoryBean().getObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return client;
+	}
+	
+	public @Bean Morphia morphia()
+	{
+		Morphia morphia = null;
+		try {
+			morphia = new MorphiaFactoryBean().getObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return morphia;
+	}
+	
+	
+	public @Bean Datastore datastore()
+	{
+		Datastore datastore = null;
+		try {
+			datastore = new DatastoreFactoryBean().getObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return datastore;
+	}
+	
+	
+	
+
+	
+
+}
